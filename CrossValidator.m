efa7ea247b95ba02f7510930c3b9e31ad2834e64
Jacob <jrw111@mun.ca>
2022-11-01 17:08:40 -0230

    Updated kfold/block
    - z-normalized features as they are divided for CV
    - can now use "all" for feature number

--- CrossValidator.m
+++ CrossValidator.m
@@ -67,9 +67,92 @@ classdef CrossValidator
                 blocks{i,1} = dataset;
             end 
         end
+        
+        function blocks = random_block_partitions(features, num_blocks)
+            arguments
+                features TrialContainer
+                num_blocks double
+            end
 
-        % TODO: consider changing this to take both train and test feature sets and
-        % returning them both here
+            indexes = 1:features.numTrials();
+            partitions = cvpartition(indexes, 'kfold', num_blocks);
+
+            for fold = 1:num_blocks
+                train_inds(:,fold) = training(partitions, fold);
+                test_inds(:,fold) = test(partitions, fold);
+            end  
+
+            blocks = cell(size(train_inds, 2), 1);
+
+            for i = 1:size(train_inds, 2)
+                train_ind = train_inds(:,i);
+                test_ind = test_inds(:,i);
+
+                [dataset.x_train, dataset.y_train] = features.getTrialsByInd(train_ind).toMatrix();
+                [dataset.x_test, dataset.y_test] = features.getTrialsByInd(test_ind).toMatrix();
+                blocks{i,1} = dataset;
+            end 
+        end
+        
+        % used for fully_random_block_classify exclusively, shouldnt be used normally
+        % results: ResultsContainer
+        function [results, varargout] = random_block(obj, classifier_func, features, num_blocks, name_val_args)
+            arguments
+                obj
+                classifier_func 
+                features TrialContainer
+                num_blocks {mustBeInteger,mustBeGreaterThan(num_blocks,1)}
+                name_val_args.num_filtered_features double = features.featureVectorLength()
+                name_val_args.runs {mustBeInteger,mustBeNonnegative} = 1
+                name_val_args.save_classifiers = false
+            end
+            num_filtered_features = name_val_args.num_filtered_features;
+            runs = name_val_args.runs;
+            save_classifiers = name_val_args.save_classifiers;
+            clear name_val_args
+
+            if save_classifiers
+                classifiers = cell(runs*num_blocks, 1);
+            end
+            for run = 1:runs
+                blocks = obj.random_block_partitions(features, num_blocks);
+                
+                temp_accuracy = zeros(num_blocks,1);
+                temp_sensitivity  = zeros(num_blocks,1);
+                temp_specificity = zeros(num_blocks,1);
+                for i = 1:num_blocks
+                    train_data = blocks{i}.x_train;
+                    test_data = blocks{i}.x_test;
+                    train_labels = blocks{i}.y_train;
+                    test_labels = blocks{i}.y_test;
+
+                    if strcmp(num_filtered_features, "all") || num_filtered_features == features.featureVectorLength()
+                        % do nothing
+                    elseif num_filtered_features ~= features.featureVectorLength()
+                        [train_data, test_data] = obj.get_best_features(train_data, train_labels, num_filtered_features, test_features=test_data);
+                    end
+
+                    train_data = normalize(train_data);
+                    test_data = normalize(test_data);
+
+                    trained_model = classifier_func(train_data, train_labels, 'OptimizeHyperparameters', 'auto',...
+                        'HyperparameterOptimizationOptions', struct('AcquisitionFunctionName','expected-improvement-plus', ...
+                        'UseParallel', obj.run_parallel, 'ShowPlots', false, 'Verbose', 0));
+                    if save_classifiers
+                        classifiers{((run-1)*num_blocks)+i,1} = trained_model;
+                    end
+                    [accuracy, sensitivity, specificity] = obj.evaluate_model(trained_model, test_data, test_labels);
+                    temp_accuracy(i,1) = accuracy;
+                    temp_sensitivity(i,1) = sensitivity;
+                    temp_specificity(i,1) = specificity;
+                end
+                results(run) = ResultsContainer(temp_accuracy, temp_sensitivity, temp_specificity);
+                if save_classifiers
+                    varargout{1} = classifiers;
+                end
+            end
+        end
+
         function [opt_features, varargout] = get_best_features(features, labels, num_feats, name_val_args)
             arguments
                 features (:,:) double
@@ -121,7 +204,7 @@ classdef CrossValidator
                 classifier_func 
                 features TrialContainer
                 num_folds {mustBeInteger,mustBeGreaterThan(num_folds,1)}
-                name_val_args.num_filtered_features double = features.featureVectorLength()
+                name_val_args.num_filtered_features = features.featureVectorLength()
                 name_val_args.runs {mustBeInteger,mustBeNonnegative} = 1
                 name_val_args.save_classifiers = false
             end
@@ -149,9 +232,15 @@ classdef CrossValidator
                     train_labels = folds{i}.y_train;
                     test_labels = folds{i}.y_test;
 
-                    if num_filtered_features ~= features.featureVectorLength()
+                    if strcmp(num_filtered_features, "all") || num_filtered_features == features.featureVectorLength()
+                        % do nothing
+                    elseif num_filtered_features ~= features.featureVectorLength()
                         [train_data, test_data] = obj.get_best_features(train_data, train_labels, num_filtered_features, test_features=test_data);
                     end
+
+                    train_data = normalize(train_data);
+                    test_data = normalize(test_data);
+
                     trained_model = classifier_func(train_data, train_labels, 'OptimizeHyperparameters', 'auto',...
                         'HyperparameterOptimizationOptions', struct('AcquisitionFunctionName','expected-improvement-plus', ...
                         'UseParallel', obj.run_parallel, 'ShowPlots', false, 'Verbose', 0));
@@ -181,7 +270,7 @@ classdef CrossValidator
                 classifier_func 
                 features TrialContainer
                 num_blocks {mustBeInteger,mustBeGreaterThan(num_blocks,1)}
-                name_val_args.num_filtered_features double = features.featureVectorLength()
+                name_val_args.num_filtered_features = features.featureVectorLength()
                 name_val_args.runs {mustBeInteger,mustBeNonnegative} = 1
                 name_val_args.save_classifiers = false
             end
@@ -209,9 +298,15 @@ classdef CrossValidator
                     train_labels = blocks{i}.y_train;
                     test_labels = blocks{i}.y_test;
 
-                    if num_filtered_features ~= features.featureVectorLength()
+                    if strcmp(num_filtered_features, "all") || num_filtered_features == features.featureVectorLength()
+                        % do nothing
+                    elseif num_filtered_features ~= features.featureVectorLength()
                         [train_data, test_data] = obj.get_best_features(train_data, train_labels, num_filtered_features, test_features=test_data);
-                    end                
+                    end   
+
+                    train_data = normalize(train_data);
+                    test_data = normalize(test_data);
+
                     trained_model = classifier_func(train_data, train_labels, 'OptimizeHyperparameters', 'auto',...
                         'HyperparameterOptimizationOptions', struct('AcquisitionFunctionName','expected-improvement-plus', ...
                         'UseParallel', obj.run_parallel, 'ShowPlots', false, 'Verbose', 0));
@@ -284,6 +379,24 @@ classdef CrossValidator
 
             features = LabelRandomizer.BlockRandomize(features);
             results = obj.kfold(classifier_func, features, num_folds, ...
+                num_filtered_features=name_val_args.num_filtered_features, ...
+                runs=name_val_args.runs, ...
+                save_classifiers=name_val_args.save_classifiers);
+        end
+        
+        function results = fully_random_block_classify(obj, classifier_func, features, num_folds, name_val_args)
+            arguments
+                obj
+                classifier_func 
+                features TrialContainer
+                num_folds {mustBeInteger,mustBeGreaterThan(num_folds,1)}
+                name_val_args.num_filtered_features double = features.featureVectorLength()
+                name_val_args.runs {mustBeInteger,mustBeNonnegative} = 1
+                name_val_args.save_classifiers = false
+            end
+
+            features = LabelRandomizer.FullRandomize(features);
+            results = obj.random_block(classifier_func, features, num_folds, ...
                 num_filtered_features=name_val_args.num_filtered_features, ...
                 runs=name_val_args.runs, ...
                 save_classifiers=name_val_args.save_classifiers);
